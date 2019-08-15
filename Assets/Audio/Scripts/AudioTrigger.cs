03d034702fd7cfe85d893e503f2c86f3b02c2fad
Filip Conic <mail@filipconic.com>
2019-08-15 15:10:54 +0200

    Created AudioManager.prefab and AudioScene for bootstrapping audio system

---
+++ Assets/Audio/Scripts/AudioTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Event = AK.Wwise.Event;
+
+public class AudioTrigger : MonoBehaviour
+{
+	public Event SoundEvent;
+
+    void Start()
+    {
+	    AkSoundEngine.PostEvent(SoundEvent.Id, gameObject);
+    }
+}
